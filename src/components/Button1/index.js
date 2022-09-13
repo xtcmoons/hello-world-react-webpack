43b68f1d2cf2762d8920c90ba3b0baa22be0921b
XTCm <xtcmoons@yeah.net>
2022-09-13 15:56:27 +0800

    多组件导出

---
+++ src/components/Button1/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+function Button1() {
+	return (
+		<div className="xtc-button1-container">
+			<button className="text">button1</button>
+		</div>
+	)
+}
+
+export default Button1
